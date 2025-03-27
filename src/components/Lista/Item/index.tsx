07a1f914cbfd2210977be92b364de5c7f0141e41
KaiqueBorgesdeOliveira <kaique.borges.1995@gmail.com>
2025-03-27 17:55:46 -0300

    feat: Várias atualizações

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
 import { Itarefa } from "../../../types/tarefa";
 
-export default function Item({ tarefa, tempo, selecionado, completado, id }: Itarefa) {
-    console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
+interface Props extends Itarefa {
+    selecionaTarefa: (tarefaSelecionada: Itarefa) => void
+}
+
+export default function Item(
+    { 
+        tarefa, 
+        tempo, 
+        selecionado, 
+        completado, 
+        id, 
+        selecionaTarefa 
+    }: Props) {
     return(
-        <li className= {style.item}>
+        <li className= {`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`} 
+        onClick={() => selecionaTarefa(
+                {
+                    tarefa,
+                    tempo,
+                    selecionado,
+                    completado,
+                    id
+                }
+            )}
+        >
         <h3>{tarefa}</h3>
         <span>{tempo}</span>
         </li>
