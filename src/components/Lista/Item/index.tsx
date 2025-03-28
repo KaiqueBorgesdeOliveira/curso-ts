6e38e9620150b47e5a323f0da995484932c6155e
KaiqueBorgesdeOliveira <kaique.borges.1995@gmail.com>
2025-03-28 10:33:17 -0300

    feat: Curso Concluído

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -17,7 +17,7 @@ export default function Item(
     }: Props) {
     return(
         <li className= {`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`} 
-        onClick={() => selecionaTarefa(
+        onClick={() => !completado && selecionaTarefa(
                 {
                     tarefa,
                     tempo,
@@ -29,6 +29,8 @@ export default function Item(
         >
         <h3>{tarefa}</h3>
         <span>{tempo}</span>
+        {completado && <span className={style.concluido}
+        aria-label="Tarefa Completada"></span>}
         </li>
     )
 }
